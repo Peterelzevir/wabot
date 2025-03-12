a388205db878430d24167e3d1faeed8e47065831
Peterelzevir <114345532+Peterelzevir@users.noreply.github.com>
2025-03-12 12:50:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const pino = require('pino');
 const fs = require('fs');
 const axios = require('axios');
 const FormData = require('form-data');
+const crypto = require('crypto');
 
 // Bot configuration
 const prefix = '.'; // Command prefix
